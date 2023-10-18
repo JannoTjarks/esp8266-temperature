480bdff0f6a196017ccfe35e442b3f9b7464c583
Janno Tjarks <janno.tjarks@mailbox.org>
2023-10-18 23:33:15 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# esp8266-temperature
+
+This code is the base of my self-developed "smart-home" temperature and 
+humidity sensor based on an ESP8266 with the DTH11. The measurments are sent
+to a mqtt server.
+
+## How does it work
+The wiring is really easy. Powered is the esp8226 with usb or by a power supply
+on the VIN pin. The dth11 sensor needs 3.3V, which can be taken from the
+esp8226. At least the data pin of the esp8226 needs to be connected on a
+gpio. For my setup i use the gpio 5.
+
+I use for the development and upload of the code `platformio`.
+
+## Configuration
+Because i use several esp8226 for different rooms, i made the code
+variablized. The configuration can be set inside the file
+`include/configuration.h`.
+This is a blueprint of the configuration.h:
+```c++
+#define SSID "<your-ssid>"
+#define WLAN_PASSWORD "<your-wifi-password>"
+
+#define DHT_PIN <number-of-the-used-gpio>
+#define DHT_TYPE <type-of-the-dht-sensor> // Possible values are DHT11 or DTH22
+
+#define MQTT_SERVER "<mqtt-server-ip-or-dns>"
+#define MQTT_HEALTH_TOPIC "<health-topic>"
+#define MQTT_TEMPERATURE_TOPIC "<temperature-topic>"
+#define MQTT_HUMIDITY_TOPIC "<humidity-topic>"
+```
---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#include <Adafruit_Sensor.h>
+#include <Arduino.h>
+#include <DHT_U.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include "configuration.h"
+
+void setup();
+void loop();
+void setup_wifi();
+void mqtt_reconnect();
+void send_measurements_to_mqtt();
+String get_temperature();
+String get_humidity();
+
+DHT_Unified dht(DHT_PIN, DHT_TYPE);
+sensors_event_t event;
+
+WiFiClient espClient;
+PubSubClient mqtt_client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+
+  setup_wifi();
+
+  dht.begin();
+
+  mqtt_client.setServer(MQTT_SERVER, 1883);
+
+  delay(10);
+}
+
+void loop() {
+  if (!mqtt_client.connected()) {
+    mqtt_reconnect();
+  }
+  mqtt_client.loop();
+
+  send_measurements_to_mqtt();
+
+  delay(30000);
+}
+
+void setup_wifi() {
+  WiFi.begin(SSID, WLAN_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void mqtt_reconnect() {
+  while (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (mqtt_client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      mqtt_client.publish(MQTT_HEALTH_TOPIC, "connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void send_measurements_to_mqtt() {
+  Serial.println("Send temperature to mqtt-server...");
+  mqtt_client.publish(MQTT_TEMPERATURE_TOPIC, get_temperature().c_str());
+  Serial.println("Done!");
+  Serial.println("Send humidity to mqtt-server...");
+  mqtt_client.publish(MQTT_HUMIDITY_TOPIC, get_humidity().c_str());
+  Serial.println("Done!");
+}
+
+String get_temperature() {
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+
+  Serial.println("Temperature: " + get_temperature() + "C");
+
+  return String(event.temperature);
+}
+
+String get_humidity() {
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+
+  Serial.println("Humidity: " + get_humidity() + "%");
+
+  return String(event.relative_humidity);
+}
