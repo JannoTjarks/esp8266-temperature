322bcfc03ed61376f9cb07ae2cb70f43a352ff41
Christian Köhlke <christian@koehlke.de>
2023-10-23 22:42:24 +0200

    converted main.cpp to DHT.h - DHT_U.h is  deprecated, according to https://github.com/adafruit/Adafruit_DHT_Unified - added check whether value is not nan before sending it to MQTT Server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Adafruit_Sensor.h>
 #include <Arduino.h>
-#include <DHT_U.h>
+#include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
@@ -11,10 +11,10 @@ void loop();
 void setup_wifi();
 void mqtt_reconnect();
 void send_measurements_to_mqtt();
-String get_temperature();
-String get_humidity();
+float get_temperature();
+float get_humidity();
 
-DHT_Unified dht(DHT_PIN, DHT_TYPE);
+DHT dht(DHT_PIN, DHT_TYPE);
 sensors_event_t event;
 
 WiFiClient espClient;
@@ -73,32 +73,39 @@ void mqtt_reconnect() {
 }
 
 void send_measurements_to_mqtt() {
-  Serial.println("Send temperature to mqtt-server...");
-  mqtt_client.publish(MQTT_TEMPERATURE_TOPIC, get_temperature().c_str());
-  Serial.println("Done!");
-  Serial.println("Send humidity to mqtt-server...");
-  mqtt_client.publish(MQTT_HUMIDITY_TOPIC, get_humidity().c_str());
-  Serial.println("Done!");
+  float temperature = get_temperature();
+  if(!isnan(temperature)){
+    Serial.println("Send temperature to mqtt-server...");
+    mqtt_client.publish(MQTT_TEMPERATURE_TOPIC, String(temperature).c_str());
+    Serial.println("Done!");
+  }
+
+  float humidity = get_humidity();
+  if(!isnan(humidity)){
+    Serial.println("Send humidity to mqtt-server...");
+    mqtt_client.publish(MQTT_HUMIDITY_TOPIC, String(humidity).c_str());
+    Serial.println("Done!");
+  }
 }
 
-String get_temperature() {
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
+float get_temperature() {
+  float temperature = dht.readTemperature();
+  if (isnan(temperature)) {
     Serial.println("Error reading temperature!");
   }
 
-  Serial.println("Temperature: " + get_temperature() + "C");
+  Serial.println("Temperature: " + String(temperature) + "C");
 
-  return String(event.temperature);
+  return temperature;
 }
 
-String get_humidity() {
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
+float get_humidity() {
+  float humidity = dht.readHumidity();
+  if (isnan(humidity)) {
     Serial.println("Error reading humidity!");
   }
 
-  Serial.println("Humidity: " + get_humidity() + "%");
+  Serial.println("Humidity: " + String(humidity) + "%");
 
-  return String(event.relative_humidity);
+  return humidity;
 }
